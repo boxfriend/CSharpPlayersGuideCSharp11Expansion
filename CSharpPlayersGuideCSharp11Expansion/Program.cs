f906ebc580acaf9d6e6e41d863e8b480dcc7eeaf
Boxfriend <cawills394@gmail.com>
2023-07-11 14:46:01 -0400

    Add project files.

---
+++ CSharpPlayersGuideCSharp11Expansion/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Title = "Program-Making Program";
+
+
+var units = Prompt("units");
+var type = Prompt("type");
+
+Console.WriteLine($"""
+
+    {GetProgramText(units, type)}
+
+    """);
+
+static string Prompt(string text)
+{
+    var result = "";
+    while(string.IsNullOrWhiteSpace(result))
+    {
+        Console.Write($"Enter the {text} used for the program: ");
+        result = Console.ReadLine();
+    }
+    return result;
+}
+static string GetProgramText(string units, string type)
+{
+    return $$"""
+        Console.WriteLine("Enter the width (in {{units}}) of the triangle: ");
+        {{type}} width = {{type}}.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the height (in {{units}}) of the triangle: ");
+        {{type}} height = {{type}}.Parse(Console.ReadLine());
+        {{type}} result = width * height / 2;
+        Console.WriteLine($"{result} square {{units}}");
+        """;
+}
