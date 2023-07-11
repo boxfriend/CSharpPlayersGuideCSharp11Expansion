ad585ad426c65d40140a8f7224bfdea9cabc1a05
Boxfriend <cawills394@gmail.com>
2023-07-11 14:49:51 -0400

    minor cleanup. removed static from method with side effects, i don't like static methods that result in side effects like that, i typically keep them as instance methods

--- CSharpPlayersGuideCSharp11Expansion/Program.cs
+++ CSharpPlayersGuideCSharp11Expansion/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine($"""
 
     """);
 
-static string Prompt(string text)
+string Prompt(string text)
 {
     var result = "";
     while(string.IsNullOrWhiteSpace(result))
@@ -20,9 +20,8 @@ static string Prompt(string text)
     }
     return result;
 }
-static string GetProgramText(string units, string type)
-{
-    return $$"""
+
+static string GetProgramText(string units, string type) => $$"""
         Console.WriteLine("Enter the width (in {{units}}) of the triangle: ");
         {{type}} width = {{type}}.Parse(Console.ReadLine());
         Console.WriteLine("Enter the height (in {{units}}) of the triangle: ");
@@ -30,4 +29,3 @@ static string GetProgramText(string units, string type)
         {{type}} result = width * height / 2;
         Console.WriteLine($"{result} square {{units}}");
         """;
-}
