951e6604abfa1038a0cbcf89d7a7c98be0fee6f9
Boxfriend <cawills394@gmail.com>
2023-07-11 17:51:58 -0400

    completed blast damage

---
+++ BlastDamage/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Numerics;
+
+Console.WriteLine(ComputeDamage(16.0d, 10.25d));
+Console.WriteLine(ComputeDamage(20f, 18f));
+Console.WriteLine(ComputeDamage(4.20m, 6.9m));
+
+static T ComputeDamage<T> (T initialDamage, T distance) where T : IMultiplyOperators<T, T, T>, IDivisionOperators<T, T, T> => initialDamage / (distance * distance);
